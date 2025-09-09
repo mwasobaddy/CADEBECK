4c28ab918107a122b521519f59c270beb6fc4c4a
mwasobaddy <kelvinramsiel@gmail.com>
2025-09-09 13:10:46 +0300

    feat: Add LeaveRequestSeeder to database seeders
    - Include LeaveRequestSeeder in the main DatabaseSeeder
    - Enable seeding of leave request test data

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -25,6 +25,7 @@ class DatabaseSeeder extends Seeder
             ContractTypeSeeder::class,
             UserSeeder::class,
             EmployeeSeeder::class,
+            LeaveRequestSeeder::class,
         ]);
     }
 }
